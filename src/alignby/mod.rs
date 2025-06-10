c9bb2e5b00b4114b7c470389b569dad8d79f45a3
Gohér László <goher.laci@gmail.com>
2025-06-10 19:43:05 +0200

    refactor alignment into separate module

---
+++ src/alignby/mod.rs
@@ -0,0 +1,70 @@
+pub struct Aligner {
+    delimiter: String,
+    lines: Vec<Line>,
+    max_index: usize,
+    max_line_number: usize,
+}
+
+impl Aligner {
+    pub fn new(delimiter: &str) -> Self {
+        Self {
+            delimiter: String::from(delimiter),
+            lines: Vec::new(),
+            max_index: 0,
+            max_line_number: 0
+        }
+    }
+
+    pub fn add_line(&mut self, line: String, line_number: usize) {
+        if let Some(index) = line.find(&self.delimiter) {
+            if index > self.max_line_number {
+                self.max_line_number = line_number;
+                self.max_index = index;
+            }
+            self.lines.push(Line {
+                text: line,
+                number: line_number,
+                delimiter_index: index,
+            })
+        }
+    }
+
+    pub fn aligned_lines<'a>(&'a self) -> AlignedLinesIterator<'a> {
+        AlignedLinesIterator {
+            aligner: self,
+            index: 0
+        }
+    }
+}
+
+
+pub struct AlignedLinesIterator<'a> {
+    aligner: &'a Aligner,
+    index: usize,
+}
+
+impl<'a> Iterator for AlignedLinesIterator<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.aligner.lines.len() {
+            let line           = &self.aligner.lines[self.index];
+            let padding_length = self.aligner.max_index - line.delimiter_index;
+            let first_half     = &line.text[..line.delimiter_index];
+            let second_half    = &line.text[line.delimiter_index..];
+            self.index += 1;
+            Some(format!("{}{}{}", first_half, " ".repeat(padding_length), second_half))
+        }
+        else {
+            None
+        }
+    }
+}
+
+
+#[derive(Debug)]
+struct Line {
+    text: String,
+    number: usize,
+    delimiter_index: usize,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,20 @@
 use std::io;
 
 
-#[derive(Debug)]
-struct Line {
-    text: String,
-    number: usize,
-    delimiter_index: usize,
-}
-
-
 fn main() -> Result<(), io::Error> {
     let stdin = io::stdin();
-    let mut lines = Vec::new();
-    let mut max_line_number = 0;
-    let mut max_index = 0;
+    let mut aligner = alignby::Aligner::new("=");
 
     for (i, maybe_line) in stdin.lines().enumerate() {
-        let line = maybe_line?;
-        if let Some(index) = line.find("=") {
-            if index > max_line_number {
-                max_line_number = i;
-                max_index = index;
-            }
-            lines.push(Line {
-                text: line,
-                number: i,
-                delimiter_index: index,
-            })
-        }
+        aligner.add_line(maybe_line?, i);
     }
 
-    for line in lines {
-        let padding_length = max_index - line.delimiter_index;
-        let first_half = &line.text[..line.delimiter_index];
-        let second_half = &line.text[line.delimiter_index..];
-        println!("{}{}{}", first_half, " ".repeat(padding_length), second_half);
+    for line in aligner.aligned_lines() {
+        println!("{}", line);
     }
 
     Ok(())
 }
+
+
+mod alignby;

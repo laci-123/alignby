1cde63e1570b83b7eaf6ea9f8afca94eda4962db
Gohér László <goher.laci@gmail.com>
2025-06-10 19:54:37 +0200

    better error reporting for error reading stdin

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use std::io;
+use std::process::ExitCode;
 
 
-fn main() -> Result<(), io::Error> {
+fn main() -> ExitCode {
     let stdin = io::stdin();
     let mut aligner = aligner::Aligner::new("=");
 
     for (i, maybe_line) in stdin.lines().enumerate() {
-        aligner.add_line(maybe_line?, i);
+        match maybe_line {
+            Ok(line) => {
+                aligner.add_line(line, i);
+            },
+            Err(err) => {
+                eprintln!("Failed to read stdin at line {}:", i);
+                eprintln!("{}", err);
+                return ExitCode::FAILURE;
+            },
+        }
     }
 
     for line in aligner.aligned_lines() {
         println!("{}", line);
     }
 
-    Ok(())
+    ExitCode::SUCCESS
 }
 
 
